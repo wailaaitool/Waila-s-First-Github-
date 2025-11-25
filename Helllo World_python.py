3893f007a6cfa8bb9b14f59830092ae9041eeb10
wailaaitool <wailaaitool@gmail.com>
2025-11-25 09:27:23 +0700

    Move hello world to function

--- Helllo World_python.py
+++ Helllo World_python.py
@@ -1,2 +1,7 @@
-print ('Hello World')
-print ('My name is Waila')
+#this is a hello world file 
+def helloworld():
+    print ('Hello World')
+    print ('My name is Waila')
+
+    helloworld()
+    
